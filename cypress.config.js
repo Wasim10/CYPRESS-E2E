ae26310688b824dd214ef43b294f8d2a13fe777f
Wasim10 <mw82777@gmail.com>
2025-09-13 15:59:18 +0500

    Fix: add supportFile to load custom commands on CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,39 @@
 const { defineConfig } = require("cypress");
+const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter');
 
 module.exports = defineConfig({
+  video: true,                        // record videos
+  videoUploadOnPasses: true,          // keep videos even on pass
+  videoCompression: 32,               // compress video size
+  screenshotsFolder: "cypress/screenshots",  
+  screenshotOnRunFailure: true,       
+
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    charts: true,
+    reportPageTitle: "My Test Suite",
+    embeddedScreenshots: true,        
+    inlineAssets: true,
+    overwrite: true,
+    html: true,
+    json: true,
+    timestamp: "mmddyyyy_HHMMss",     
+    quiet: false,
+    reportFilename: "my_custom_report",
+    saveAllAttempts: true
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
+    supportFile: "cypress/support/e2e.js",   // ✅ tell Cypress to load custom commands
   },
-  env:{
-    URL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
+
+  env: {
+    RegisterationURL: "https://naveenautomationlabs.com/opencart/index.php?route=account/register",
+    LoginURL: "https://naveenautomationlabs.com/opencart/index.php?route=account/login"
   }
 });
